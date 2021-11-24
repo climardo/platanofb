03cf0eb235698595c331f6827a5339572cc2fa36
Carlos Limardo <climardo@gmail.com>
2021-11-24 16:02:18 -0500

    Fix week 12 data

--- _includes/default.js
+++ _includes/default.js
@@ -1,41 +1,41 @@
 function btnChg(color, btn) {
-    btn.style.background = color;
+    btn.style.background = color
 }
 
 function setBg() {
     if ('{{ page.header_image }}' !== '') {
-        let header = document.getElementById('site-header');
-        header.style.backgroundImage = "url('{{ page.header_image }}')";
-        header.style.backgroundPosition = "{{ page.header_pos }}";
+        let header = document.getElementById('site-header')
+        header.style.backgroundImage = "url('{{ page.header_image }}')"
+        header.style.backgroundPosition = "{{ page.header_pos }}"
     }
 }
 
 if ('{{ page.layout }}' === 'post' && '{{ page.header_image }}' !== 'nil') {
-    document.addEventListener('load', setBg());
+    document.addEventListener('load', setBg())
 }
 
 function hideCountdown() {
-    var d = new Date();
-    var day = new Date().getDay();
+    var d = new Date()
+    var day = new Date().getDay()
     try {
         if (d > countDownDate || day < 2) {
-            document.getElementById("draft").classList.add("d-none");
-            let contestLinks = document.getElementsByClassName("contest-link");
+            document.getElementById("draft").classList.add("d-none")
+            let contestLinks = document.getElementsByClassName("contest-link")
             for (var i = 0; i < contestLinks.length; i++) {
-                let origLink = contestLinks[i].getAttribute("href");
-                let newLink = origLink.replace("draft/contest", "contest/gamecenter");
-                contestLinks[i].setAttribute("href", newLink);
+                let origLink = contestLinks[i].getAttribute("href")
+                let newLink = origLink.replace("draft/contest", "contest/gamecenter")
+                contestLinks[i].setAttribute("href", newLink)
             }
             let links = document.getElementsByClassName("nav-link")
             let contestMenuLink = document.getElementById("submit")
 
-            let origMenuLink = contestMenuLink.getAttribute("href");
-            let newMenuLink = origMenuLink.replace("draft/contest", "contest/gamecenter");
-            contestMenuLink.setAttribute("href", newMenuLink);
+            let origMenuLink = contestMenuLink.getAttribute("href")
+            let newMenuLink = origMenuLink.replace("draft/contest", "contest/gamecenter")
+            contestMenuLink.setAttribute("href", newMenuLink)
             contestMenuLink.innerHTML = "Weekly contest <i class='fas fa-football-ball'></i>"
         } else {
             try {
-                document.getElementById("contest").classList.add("d-none");
+                document.getElementById("contest").classList.add("d-none")
             }
             catch (err) {
                 console.log('Nothing to hide')
@@ -46,7 +46,7 @@ function hideCountdown() {
     }
 }
 
-document.addEventListener('load', hideCountdown());
+document.addEventListener('load', hideCountdown())
 
 let playersOnPage = []
 let boldItems = document.getElementsByTagName("strong")
